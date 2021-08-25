32e748c6a4fbe05b9f3b67f76be5f48a4937b619
Ato Araki <ato.araki@gmail.com>
2021-08-25 10:58:49 +0900

    Update fastwalk

--- fastwalk/fastwalk_portable.go
+++ fastwalk/fastwalk_portable.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build appengine || (!linux && !darwin && !freebsd && !openbsd && !netbsd)
 // +build appengine !linux,!darwin,!freebsd,!openbsd,!netbsd
 
 package fastwalk
@@ -18,10 +19,18 @@ import (
 func readDir(dirName string, fn func(dirName, entName string, typ os.FileMode) error) error {
 	fis, err := ioutil.ReadDir(dirName)
 	if err != nil {
-		return nil
+		return err
 	}
+	skipFiles := false
 	for _, fi := range fis {
+		if fi.Mode().IsRegular() && skipFiles {
+			continue
+		}
 		if err := fn(dirName, fi.Name(), fi.Mode()&os.ModeType); err != nil {
+			if err == ErrSkipFiles {
+				skipFiles = true
+				continue
+			}
 			return err
 		}
 	}
